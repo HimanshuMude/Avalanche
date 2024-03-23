3b66a9ef4836308e472ed51c426bac69eade40d6
Sahil Shile <sahilshile25@gmail.com>
2024-03-24 04:25:10 +0530

    project Done

---
+++ Backend/api/getTranscript.js
@@ -0,0 +1,38 @@
+const ytdl = require("ytdl-core");
+const fs = require("fs");
+const openAiWhisper = require("./openaiWhisper");
+const Transcripts = require("../models/Transcripts");
+
+const getTranscriptResponse = async (url, mp3) => {
+  try {
+    const foundVideo = await Transcripts.findOne({ videoUrl: url });
+    if (foundVideo) {
+      return { success: true, newFileCreated: false, transcriptText: foundVideo.transcript };
+    } else {
+      const info = await ytdl.getInfo(url);
+      if(info.videoDetails.lengthSeconds > 1380){
+        return { success: false,  newFileCreated: false, error: "Video is larger than expected, try smaller videos" };
+      }
+      const headerText = `The title is: ${info.videoDetails.title} and the text is: `
+      const video = ytdl(url, {quality: "lowestaudio"}, { filter: "audioonly" });
+      const writeStream = fs.createWriteStream(mp3);
+      video.pipe(writeStream);
+      const fileCreationPromise = new Promise((resolve) => {
+        writeStream.on("finish", () => {
+          resolve();
+        });
+      });
+      await fileCreationPromise;
+
+      const transcriptText = await openAiWhisper(url, mp3, headerText);
+      if(transcriptText.error){
+        return { success: false,  newFileCreated: false, error: transcriptText.error };
+      }
+      return { success: true, newFileCreated: true, transcriptText: transcriptText.resp, mongoId: transcriptText.createdMongoId };
+    }
+  } catch (error) {
+    return { success: false,  newFileCreated: false, error: error.message };
+  }
+};
+
+module.exports = getTranscriptResponse;
---
+++ Frontend/src/components/Home/HeroSection.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import classes from "./HeroSection.module.css";
+import heroImage from "../../assets/Bhavik.png";
+import TypeIt from "typeit-react";
+import { TypeAnimation } from 'react-type-animation';
+import { useState } from "react";
+
+const HeroSectionComponent = (props) => {
+  const [textColor, setTextColor] = useState('red');
+  const CURSOR_CLASS_NAME = 'custom-type-animation-cursor';
+  const [typingStatus, setTypingStatus] = useState('Initializing');
+
+  return (
+    <div className={classes.hero} id={props.id}>
+      <div className={classes["hero-container"]}>
+        <div className={classes.texts}>
+          <div className={classes["gradient-span"]}
+            style={{
+              fontSize: '40px',
+            }}
+          >
+            <TypeAnimation
+              sequence={[
+                'Join the AI Revolution with',
+                800,
+                () => setTextColor('red'),
+                'StreamSight',
+                800,
+                () => setTextColor('aqua'),
+              ]}
+              repeat={Infinity}
+            />
+          </div>
+          <p>
+            Discover the smarter way to explore Youtube videos with our advance transcription technology. You can teach and learn from your own bot and get instant answer to your video queries. Plus we are always working on new updates and features to make your video exploration even better. Stay tuned!
+          </p>
+          {/* <button className={classes.buttons}>Try Now</button> */}
+        </div>
+        <div className={classes["image-container"]}>
+          <img src={heroImage} alt="" height="500px"/>
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default HeroSectionComponent;
