33d24708b0b0fb66807b2610686026476c1d489c
Sahil Shile <100942861+sahil7264@users.noreply.github.com>
2024-03-24 04:51:36 +0530

    Update QnaComponent.jsx

--- Frontend/src/components/Qna/QnaComponent.jsx
+++ Frontend/src/components/Qna/QnaComponent.jsx
@@ -27,7 +27,7 @@ const QnaComponent = (props) => {
       setLoading(true);
       setAnswer(null);
       setError(null);
-      const response = await axios.post("http://localhost:5000/api/qna", {
+      const response = await axios.post("https://avalanche-ffp5.onrender.com/api/qna", {
         query: question,
       });
       setLoading(false);
