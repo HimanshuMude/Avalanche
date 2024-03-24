a4ba9e887d24ab61ee7dc915e0f04341c9003012
HimanshuMude <98302342+HimanshuMude@users.noreply.github.com>
2024-03-24 10:52:01 +0530

    Update UploadComponent.jsx

--- Frontend/src/components/Upload/UploadComponent.jsx
+++ Frontend/src/components/Upload/UploadComponent.jsx
@@ -37,7 +37,7 @@ const UploadComponent = (props) => {
       setSuccess(null);
       // Set the origin header to your server's domain
       const response = await axios.post(
-        "https://avalanche-ffp5.onrender.com/api/transcript",
+        "https://avalanche-mp2k.onrender.com/api/transcript",
         {
           url: enteredLink,
         }
